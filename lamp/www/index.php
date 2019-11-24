7957ab7ea90f89c9c772066fd65ac91032a2a58f
Olaf Sarnow <olsarnow@gmail.com>
2019-11-24 12:53:37 +0100

    mysql index.php

--- lamp/www/index.php
+++ lamp/www/index.php
@@ -1,13 +1,15 @@
-<h1>Hello World</h1>
-<h4>Attempting MySQL connection from php...</h4>
-<!--?php 
-$host = 'mysql';
-$user = 'root';
-$pass = 'rootpassword';
-$conn = new mysqli($host, $user, $pass);
+<?php
+$link = mysqli_connect("mysql", "root", "rootpassword");
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected to MySQL successfully!";
-?-->
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+mysqli_close($link);
+?>
